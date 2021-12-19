981a2513442282063aa23699fbb5bd6c8efcd4ae
CircuitCoder-ngh <96142377+CircuitCoder-ngh@users.noreply.github.com>
2021-12-19 15:04:17 -0500

    Updated snake_game.py

--- snake_game.py
+++ snake_game.py
@@ -1,12 +1,13 @@
 import time
 import machine
-import config
 import ssd1306
 import random
 
 # config variables
-BUTTON1_PIN =
-BUTTON2_PIN =
+DISPLAY_SCL_PIN = 22
+DISPLAY_SDA_PIN = 21
+BUTTON1_PIN = 27
+BUTTON2_PIN = 12
 oled_width = 128
 oled_height = 64
 
@@ -15,8 +16,8 @@ button1 = machine.Pin(BUTTON1_PIN, machine.Pin.IN, machine.Pin.PULL_UP)
 button2 = machine.Pin(BUTTON2_PIN, machine.Pin.IN, machine.Pin.PULL_UP)
 
 # initialize screen
-i2c = machine.I2C(-1,scl=machine.Pin(config.DISPLAY_SCL_PIN),
-                  sda=machine.Pin(config.DISPLAY_SDA_PIN))
+i2c = machine.SoftI2C(scl=machine.Pin(DISPLAY_SCL_PIN),
+                  sda=machine.Pin(DISPLAY_SDA_PIN))
 display = ssd1306.SSD1306_I2C(oled_width, oled_height, i2c) 
 
 # global variables
@@ -27,24 +28,15 @@ length = 1
 running = False
 apple_position = 0
 
-def th(x):
+def th(timer):
     # for this program we can use one buttonCounter for both buttons
     # since the user can only turn one direction at a time anyway
     global buttonCounter
     if buttonCounter == 0:
         buttonCounter += 1
-    if obj.running == False:
-        if button1.value() == 0:
-            obj.p_list = [(64,32)]
-            obj.direction = 'up'
-            obj.running = True
-        elif button2.value() == 0:
-            obj.p_list = [(64,32)]
-            obj.direction = 'up'
-            obj.running = True
 
 timer = machine.Timer(-1)
-timer.init(period=200, mode=machine.Timer.PERIODIC, callback=th)
+timer.init(period=400, mode=machine.Timer.PERIODIC, callback=th)
 
 # creates new position coordinates using direction and last position
 def update_position():
@@ -59,14 +51,14 @@ def update_position():
     elif direction == 'down':
         new_xy = (last_xy[0], last_xy[1] - 1)
 
-    if new_xy in xy_list:
+    if new_xy in xy_list: # game over if you hit yourself
         game_over()
     else:
         xy_list.append(new_xy)
 
 # generates apple coordinates
 def generate_apple():
-    global apple_pos
+    global apple_position
     x = random.randint(1,127)
     y = random.randint(16,63)
     apple_position = (x,y)
@@ -92,9 +84,9 @@ def apple_check():
     if len(xy_list) > length:
         xy_list.pop(0)
 
-# if game is not running, checks buttons to start game...otherwise,
-# button1 turns user left (relative to current direction), and button2
-# turns user right (relative to current direction)
+# if game is not running, check buttons to start game...otherwise,
+# button1 turns user left (relative to current direction),
+# and button2 turns user right (relative to current direction)
 def check_buttons():
     global buttonCounter
     global direction
@@ -147,12 +139,12 @@ def check_buttons():
 # updates display to show current position of 'snake'
 def update_display():
     display.fill(0)
-    for x in range(0, len(xy_list)):
-        point = xy_list[x]
+    for i in range(0, len(xy_list)):
+        point = xy_list[i]
         display.pixel(point[0],point[1],1)
     display.rect(0,16,128,48,1)
     display.pixel(apple_position[0],apple_position[1],1)
-    display.text('Score: ' + str(self.length - 1), 12,4,1)
+    display.text('Score: ' + str(length - 1), 12,4,1)
     if running == False:
         display.text('Click button',12,25,1)
         display.text('to begin',15,35,1)
@@ -164,13 +156,16 @@ def game_over():
     if running == True:
         running = False
         display.fill_rect(12,20,104,40,1)
-        display.text('Game Over: ' + str(length),15,30,0)
+        display.text('Game Over: ' + str(length -1),15,30,0)
         display.show()
         reset_variables()
         time.sleep(2)
 
 # reset all starting variables
 def reset_variables():
+    global xy_list
+    global direction
+    global length
     xy_list = [(64,32)] 
     direction = 'up'
     length = 1
@@ -190,6 +185,5 @@ while True:
     while running == False:
         check_buttons()
         update_display()
-
 
     
